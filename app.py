97d8c48382a19fe24d363b4bfd07b44c0432b79f
aculeasis <42580940+Aculeasis@users.noreply.github.com>
2018-08-22 02:29:43 +0300

    Docker: added dockerfile for arm64v8 App: added new option format. Use format=<wav|mp3|opus>, mp3 as default.

--- app.py
+++ app.py
@@ -1,29 +1,58 @@
-from flask import Flask, request, send_from_directory, after_this_request
-import subprocess, os, urllib, hashlib, re
-from tools.preprocessing.text_prepare import text_prepare
+import subprocess
+from io import BytesIO
 from shlex import quote
+from urllib import parse
+
+from flask import Flask, request, send_file, make_response, abort
+
+from tools.preprocessing.text_prepare import text_prepare
+
+SUPPORT_VOICES = {
+    'aleksandr', 'anna', 'elena', 'irina',  # Russian
+    'alan', 'bdl', 'clb', 'slt',  # English
+    'spomenka',  # Esperanto
+    'natia',  # Georgian
+    'azamat', 'nazgul',  # Kyrgyz
+    'talgat',  # Tatar
+    'anatol', 'natalia'  # Ukrainian
+}
+DEFAULT_VOICE = 'anna'
+
+FORMATS = {
+    'mp3':  ['echo {text} | RHVoice-test -p {voice} -o - | lame -h -V 4 - -',           'audio/mpeg'],
+    'wav':  ['echo {text} | RHVoice-test -p {voice} -o -',                              'audio/wav'],
+    'opus': ['echo {text} | RHVoice-test -p {voice} -o - | opusenc --ignorelength - -', 'audio/ogg'],
+}
+DEFAULT_FORMAT = 'mp3'
 
 app = Flask(__name__, static_url_path='')
-data_path = "/opt/data"
 
 
 @app.route('/say')
 def say():
-    text = re.sub('[\r\n]',' ',text_prepare(urllib.parse.unquote(request.args.get('text'))))
-    m = hashlib.md5()
-    m.update(text.encode('utf-8'))
-    file_name = m.hexdigest()
-    file_path = data_path + "/" + file_name
-    voice = request.args.get('voice')
-    voice = "-p anna" if voice is None else "-p %s" % (voice)
-    cmd = "echo %s | RHVoice-test %s -o %s.wav && lame %s.wav %s.mp3" % (quote(text), voice, file_path, file_path, file_path)
-    subprocess.call([cmd], shell=True)
-    @after_this_request
-    def remove_file(response):
-        os.remove("%s.wav" % (file_path))
-        os.remove("%s.mp3" % (file_path))
-        return response
-    return send_from_directory(data_path, "%s.wav" % (file_name))
+    text = request.args.get('text', '')
+    voice = request.args.get('voice', DEFAULT_VOICE)
+    format_ = request.args.get('format', DEFAULT_FORMAT)
+
+    if voice not in SUPPORT_VOICES:
+        return make_response('Unknown voice: \'{}\'. Support: {}.'.format(voice, ', '.join(SUPPORT_VOICES)), 400)
+    if format_ not in FORMATS:
+        return make_response('Unknown format: \'{}\'. Support: {}.'.format(format_, ', '.join(FORMATS)), 400)
+    if not text:
+        return make_response('Unset \'text\'.', 400)
+
+    text = quote(text_prepare(parse.unquote(text).replace('\r\n', ' ').replace('\n', ' ')))
+    run = subprocess.run(
+        FORMATS[format_][0].format(text=text, voice=voice),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True
+    )
+    if len(run.stdout) > 42:  # any size
+        return send_file(filename_or_fp=BytesIO(run.stdout), mimetype=FORMATS[format_][1])
+    else:
+        abort(500)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
