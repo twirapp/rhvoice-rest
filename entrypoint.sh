e706e17a28b1db3ecc9e212586894c84a82fac72
aculeasis <42580940+Aculeasis@users.noreply.github.com>
2018-09-08 05:40:21 +0300

    remove VOLUME

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,15 @@ cleanup() {
     exit 0
 }
 
+if [ ! -f /opt/cfg/configured ]; then
+    if [ -f /usr/local/etc/RHVoice/RHVoice.conf ] && [ ! -L /usr/local/etc/RHVoice/RHVoice.conf ]; then
+        mv /usr/local/etc/RHVoice/RHVoice.conf /opt/cfg/RHVoice.conf
+        ln -s /opt/cfg/RHVoice.conf /usr/local/etc/RHVoice/RHVoice.conf
+    fi
+
+    touch /opt/cfg/configured
+fi
+
 if [ -f /opt/app.py ]; then
     python3 /opt/app.py &
 fi
