5f4ae4ef0a61c2edc1f4bcbde58c2635480e0717
aculeasis <42580940+Aculeasis@users.noreply.github.com>
2018-09-10 05:21:53 +0300

    fix lost links

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,11 @@ cleanup() {
     exit 0
 }
 
-if [ ! -f /opt/cfg/configured ]; then
-    if [ -f /usr/local/etc/RHVoice/RHVoice.conf ] && [ ! -L /usr/local/etc/RHVoice/RHVoice.conf ]; then
+if [ -f /usr/local/etc/RHVoice/RHVoice.conf ] && [ ! -L /usr/local/etc/RHVoice/RHVoice.conf ]; then
+    if [ ! -f /opt/cfg/RHVoice.conf ]; then
         mv /usr/local/etc/RHVoice/RHVoice.conf /opt/cfg/RHVoice.conf
-        ln -s /opt/cfg/RHVoice.conf /usr/local/etc/RHVoice/RHVoice.conf
     fi
-
-    touch /opt/cfg/configured
+    ln -fs /opt/cfg/RHVoice.conf /usr/local/etc/RHVoice/RHVoice.conf
 fi
 
 if [ -f /opt/app.py ]; then
