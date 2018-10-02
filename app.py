f97e449c459094fa95b3ea02f302d86dc6263453
aculeasis <42580940+Aculeasis@users.noreply.github.com>
2018-10-02 03:00:07 +0300

    Up

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import os
-import shutil
 from shlex import quote
 from urllib import parse
 
@@ -21,8 +19,8 @@ SUPPORT_VOICES = {
 }
 DEFAULT_VOICE = 'anna'
 
-FORMATS = {'wav': 'audio/wav'}
-DEFAULT_FORMAT = 'wav'
+FORMATS = {'wav': 'audio/wav', 'mp3': 'audio/mpeg', 'opus': 'audio/ogg'}
+DEFAULT_FORMAT = 'mp3'
 
 app = Flask(__name__, static_url_path='')
 
@@ -49,46 +47,22 @@ def say():
     return Response(stream_with_context(stream_()), mimetype=FORMATS[format_])
 
 
-def format_support():
-    base_cmd = {
-        'mp3': [['lame', '-htv', '--silent', '-', '-'], 'lame', 'audio/mpeg', 'LAMEPATH'],
-        'opus': [
-            ['opusenc', '--quiet', '--discard-comments', '--ignorelength', '-', '-'],
-            'opus-tools', 'audio/ogg', 'OPUSENCPATH'
-        ]
-    }
-    cmd_ = {}
-    formats = {'wav': 'audio/wav'}
-    for key, val in base_cmd.items():
-        bin_path = os.environ.get(val[3]) or val[0][0]
-        if shutil.which(bin_path):
-            cmd_[key] = val[0]
-            cmd_[key][0] = bin_path
-            formats[key] = val[2]
-        else:
-            print('Disable {} support - {} not found. Use apt install {}'.format(key, bin_path, val[1]))
-    return cmd_, formats
-
-
-def get_path():
-    variables = ['RHVOICELIBPATH', 'RHVOICEDATAPATH', 'RHVOICERESOURCES']
-    return [os.environ.get(x) for x in variables]
-
-
-def get_threads():
-    try:
-        cont = int(os.environ.get('THREADED', 1))
-    except ValueError:
-        cont = 1
-    return cont if cont > 0 else 1
+def _get_def(any_, test):
+    if test not in any_ and len(any_):
+        return any_[0]
+    return test
 
 
 if __name__ == "__main__":
-    (cmd, FORMATS) = format_support()
-    if 'mp3' in FORMATS:
-        DEFAULT_FORMAT = 'mp3'
-    threads = get_threads()
-    paths = get_path()
-    (tts, SUPPORT_VOICES) = TTS(cmd, threads=threads, lib_path=paths[0], data_path=paths[1], resources=paths[2])
-    app.run(host='0.0.0.0', port=8080, threaded=threads > 1)
+    tts = TTS()
+
+    formats = tts.formats
+    DEFAULT_FORMAT = _get_def(formats, DEFAULT_FORMAT)
+    FORMATS = {key: val for key, val in FORMATS.items() if key in formats}
+
+    SUPPORT_VOICES = tts.voices
+    DEFAULT_VOICE = _get_def(SUPPORT_VOICES, DEFAULT_VOICE)
+    SUPPORT_VOICES = set(SUPPORT_VOICES)
+
+    app.run(host='0.0.0.0', port=8080, threaded=tts.thread_count > 1)
     tts.join()
